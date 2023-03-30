dd510afdeb6113dc3f3fe74594d47e153e1aa966
Lyssenko Alexandr <lyssenko.alex@gmail.com>
2023-03-30 23:04:26 +0600

    feat:add merch block

--- components/MerchGeneral/merch-general.tsx
+++ components/MerchGeneral/merch-general.tsx
@@ -54,17 +54,20 @@ export const MerchGeneral: FC<MerchGeneralProps> = ({
               >
                 <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
                   <div className="flex items-center space-x-6 lg:space-x-8">
-                    {imagesList && getImageBlock([imagesList[0], imagesList[1]])}
-                    {imagesList && getImageBlock([
-                      imagesList[3],
-                      imagesList[2],
-                      imagesList[4]
-                    ])}
-                    {imagesList && getImageBlock([
-                      imagesList[5],
-                      imagesList[6],
-                      imagesList[7]
-                    ])}
+                    {imagesList &&
+                      getImageBlock([imagesList[0], imagesList[1]])}
+                    {imagesList &&
+                      getImageBlock([
+                        imagesList[3],
+                        imagesList[2],
+                        imagesList[4]
+                      ])}
+                    {imagesList &&
+                      getImageBlock([
+                        imagesList[5],
+                        imagesList[6],
+                        imagesList[7]
+                      ])}
                   </div>
                 </div>
               </div>
