b18fcde3fbb623b2038143bb26f879f609111c04
qeteq <qeteq0@gmail.com>
2023-04-11 21:12:49 +0400

    fix: ts errors

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 
-import { resolveProductionUrl } from './resolveProductionUrl';
 import { schemaTypes } from './schemas';
 
 const projectId = import.meta.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
